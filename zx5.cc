0b1871057a6b0c94e2dd80fde26391400a1ea1d6
zhangxin <1514962740@qq.com>
2022-11-10 15:55:31 +0800

    2022-11-10-15-55-31

--- zx5.cc
+++ zx5.cc
@@ -2,21 +2,21 @@
 #include <Eigen/Core>
 using namespace std;
 using namespace Eigen;
-
 class biCGstab
+// Stable Biconjugate Gradient Method
 {
 public:
-    biCGstab(int max4loop_ = 1e2,
-             int nu4dim_ = 44,
-             float min4diff_ = 1e-4,
-             MatrixXd *poit4A_ = NULL,
-             VectorXd *poit4b_ = NULL)
+    biCGstab(int max4loop = 1e2,
+             int nu4dim = 44,
+             float min4diff = 1e-4,
+             MatrixXd *poit4A = NULL,
+             VectorXd *poit4b = NULL)
     {
-        max4loop = max4loop_;
-        nu4dim = nu4dim_;
-        min4diff = min4diff_;
-        poit4A = poit4A_;
-        poit4b = poit4b_;
+        this->max4loop = max4loop;
+        this->nu4dim = nu4dim;
+        this->min4diff = min4diff;
+        this->poit4A = poit4A;
+        this->poit4b = poit4b;
     };
     struct values2out
     {
@@ -44,7 +44,7 @@ public:
         delete poit;
     };
 
-private:
+protected:
     int max4loop;
     int nu4dim;
     float min4diff;
@@ -89,7 +89,7 @@ private:
             //  << endl
             //  << ri << endl
             //  << "\n*************************************\n"
-             ;
+            ;
     };
     void Compare()
     {
@@ -107,6 +107,7 @@ private:
         }
     };
     void Calculate()
+    // Refer to https://zh.m.wikipedia.org/wiki/%E7%A8%B3%E5%AE%9A%E5%8F%8C%E5%85%B1%E8%BD%AD%E6%A2%AF%E5%BA%A6%E6%B3%95.
     {
         loop += 1;
         double &proI = tmp4double;
@@ -147,12 +148,82 @@ private:
         //      << xi << "\n*************************************\n";
     };
 };
+
+class biCGstabX : protected biCGstab
+// Going to be Preconditioned Stable Biconjugate Gradient Method...
+{
+public:
+    using biCGstab::_values2out;
+    using biCGstab::biCGstab;
+    using biCGstab::del4values2out;
+    void del4biCGstab(biCGstabX *poit)
+    {
+        poit = NULL;
+        delete poit;
+    };
+    void Run()
+    {
+        Config();
+        while (loop + 1)
+        {
+            Calculate();
+        };
+    };
+
+private:
+    using biCGstab::Calculate;
+    using biCGstab::Compare;
+    using biCGstab::Config;
+    void Calculate()
+    // Refer to https://zh.m.wikipedia.org/wiki/稳定双共轭梯度法
+    {
+        loop += 1;
+        double &proI = tmp4double;
+        proI = proi;
+
+        double &proi1 = proi;
+        proi1 = r0.adjoint() * ri;
+
+        double &beta = tmp4double;
+        beta = (proi1 / proI) * (alpha / wi);
+
+        VectorXd &pi1 = pi;
+        pi1 = ri + beta * (pi - wi * vi);
+
+        VectorXd &vi1 = vi;
+        vi1 = A * pi1;
+
+        alpha = proi1 / (r0.adjoint() * vi1);
+        VectorXd &s = ri;
+        s = ri - alpha * vi1;
+
+        double &wi1 = wi;
+        tmp4double = (A * s).adjoint() * s;
+        wi1 = tmp4double / ((A * s).adjoint() * (A * s));
+
+        VectorXd &xi1 = xi;
+        xi1 = xi + alpha * pi1 + wi1 * s;
+        Compare();
+        VectorXd &ri1 = ri;
+        ri1 = s - wi1 * (A * s);
+        //     cout << "w(" << loop << "):" << endl
+        //          << wi << "\n*************************************\n"
+        //          << "a(" << loop << "):" << endl
+        //          << alpha << "\n*************************************\n"
+        //          << "r(" << loop << "):" << endl
+        //          << ri << "\n*************************************\n"
+        //          << "x(" << loop << "):" << endl
+        //          << xi << "\n*************************************\n";
+        //
+    };
+};
 int main()
 {
-    biCGstab bi(1e5, 10, 1e-10);
+    biCGstab bi(1e5, 30, 1e-5);
     bi.Run();
-    biCGstab::values2out values  = bi._values2out;
+    biCGstab::values2out values = bi._values2out;
     cout << values.diff << endl;
     bi.del4values2out(&values);
     bi.del4biCGstab(&bi);
+    return 0;
 }
