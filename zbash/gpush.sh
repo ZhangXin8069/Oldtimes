33025a05885cd5f596e8781d1c857b6a243a2060
zhangxin <zhangxin8069@qq.com>
2023-03-02 13:59:36 +0800

    2023-03-02-13-59-36

--- zbash/gpush.sh
+++ zbash/gpush.sh
@@ -1,1 +1,1 @@
-cd ~/content && git add * && git commit -m $(date "+%Y-%m-%d-%H-%M-%S") && echo 'content have git_push successful'、
+cd ~/content && git add * && git commit -m $(date "+%Y-%m-%d-%H-%M-%S") && git push && echo 'content have git_push successful'、
