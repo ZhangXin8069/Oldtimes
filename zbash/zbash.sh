1e65135bb4245467a3a3bf354d62952ce6f4525e
zhangxin <zhangxin8069@qq.com>
2023-03-11 14:12:07 +0800

    2023-03-11-14-12-07

--- zbash/zbash.sh
+++
@@ -1,4 +0,0 @@
-cd ~/content && mkdir -p zbash && mv z*\.sh 'zbash' && cp ~/.bashrc 'zbash'$(date "+%Y-%m-%d-%H-%M-%S")	
-mkdir -p ~/content/zbash 
-mv ~content/'z*\.sh' ~/content/zbash 
-cp ~/.bashrc ~/content/zset	
